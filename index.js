b31a067e8eb56c94a3799b98ad8b5414d4701621
Tao <werewaswas@live.com>
2018-06-19 05:58:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,13 +9,35 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
+function addToCart(itemName) {
  // write your code here
+ const itemPrice =  Math.floor(100 * Math.random());
+ const item = {itemName: itemName, itemPrice: itemPrice};
+ cart = [...cart, item];
+ console.log(`${itemName} has been added to your cart.`);
+ return `${itemName} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
-}
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  }
+  let message = `In your cart, you have`;
+  return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`;
+
+//   cart.map ((item, i) => {
+//     message =+ ` ${item.itemName} at $${item.itemPrice}`
+//     if (i == cart.length - 2) {
+//       message =+ `, and`
+//     } else if (i == cart.length - 1) {
+//       message =+ `.`
+//     } else {
+//       message =+ `,`
+//     }
+//   })
+//     return message;
+// }
 
 function total() {
   // write your code here
