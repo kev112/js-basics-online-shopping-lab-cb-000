4e30343492fd87a9fa01d051bbb80ed7624769ae
Tao <werewaswas@live.com>
2018-06-19 06:31:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for (let i = 0; i < cart.length; i++) {
-    if (item == cart[i]) {
+    if (item == cart[i].itemName) {
       cart.splice(i, 1);
     }
   }
